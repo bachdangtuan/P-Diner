9f31e7a497a9f23e54a212364cba71248f88b5e9
Bach <bachdangtuan93@gmail.com>
2021-10-01 16:17:08 +0700

    Navbar + Carousel v1

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+dinerHeader();
+function dinerHeader() {
+     document.getElementById("Dinner__logo").style.cssText = "font-size:36px; font-weight:700;"
+     document.getElementById("Dinner__search").style.cssText = "right:15px"
+}
